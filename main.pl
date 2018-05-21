51675c80dea9bd70fd8955a3c6ea971f18d8309b
MartinCJ08 <martin.carracoj@gmail.com>
2018-05-20 20:47:26 -0500

    Mejora de conocimiento, motor de inferencia y agregación de imagenes

--- main.pl
+++ main.pl
@@ -1,5 +1,9 @@
-consult('motorInferencia.pl').
-consult('.AgroSistemaExperto/conocimiento.pl').
+:-use_module(library(pce)).
+:-consult(conocimiento).
+:-pce_image_directory('./img').
+resource(logo,image,image('logo.jpg')).
+
+%% tp(N1,R):-tipoSiembra(N1,L),send(R,selection,L).
 mes:-write('Push it').
 
 plantar:-new(Dp,dialog('Vamos a plantar!')),
@@ -8,21 +12,23 @@ new(TxtPrueba,text_item('Prueba')),
 new(BtnHor,button('Buscar',message(@prolog,mes))),
 %% N1 is TxtExt,
 %% tp(N1,R),
+%% write(R),
 send_list(Dp,append,[TxtExt,TxtPrueba,BtnHor]),
 
 send(Dp,open).
 
-plantado:-new(Dp,dialog('Titulo')),
-send(B,append,[chile,acelga]),
-send(B,append,Dp),
-send(Dp,open).
+plantado:-new(Dpl,dialog('Titulo')),
+%% send(B,append,[chile,acelga]),
+%% send(B,append,Dpl),
+send(Dpl,open).
 
 
-main:-new(D,dialog('Sistema Experto acerca de Horalizas')),
+main:-new(LblIcon,label(icon,resource(logo))),
+new(D,dialog('Sistema Experto acerca de Horalizas')),
 new(BtnA,button('Quieres plantar?',message(@prolog,plantar))),
 new(BtnB,button('Ya tengo plantado!',message(@prolog,plantado))),
 
-send_list(D,append,[BtnA,BtnB]),
+send_list(D,append,[LblIcon,BtnA,BtnB]),
 send(BtnB,below,BtnA),
 send(D,open).
 
--- motorInferencia.pl
+++ motorInferencia.pl
@@ -1,3 +1,17 @@
-%% tp(N1,R):-tipoSiembra(),member(),send(R,,P). si uso una lista de palabras clave
-%%Motor inferencia para buscar tipo de Siembra
-tp(N1,R):-tipoSiembra(N1,L),send(R,selection,L).
+tp(H,R):-tipoSiembra(H,L),write(L),send(R,selection,L).
+
+prof(H,R):-profundidad(H,L),send(R,selection,L).
+
+dist(H,Planta,Hilera):-distancia(H,P,Hl),
+send(Planta,selection,P),send(Hilera,selection,Hl).
+vent:-consult('./AgroSistemaExperto/conocimiento.pl'),
+new(D,dialog('Prueba')),
+new(TxtA,text_item('Hortaliza')),
+new(TxtB,text_item('Planta')),
+new(TxtC,text_item('Hilera')),
+
+new(BtnA,button('Pushale',message(@prolog,dist,TxtA?selection,TxtB,TxtC))),
+
+send_list(D,append,[TxtA,TxtB,TxtC,BtnA]),
+
+send(D,open).
