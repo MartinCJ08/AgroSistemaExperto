3fc22eb17f4867512cf1f4244e7f9c144bd809d1
MartinCJ08 <martin.carracoj@gmail.com>
2018-05-26 02:37:59 -0500

    Motor de inferencia ampliado, cambio de logo y pendiente lista

--- main.pl
+++ main.pl
@@ -1,29 +1,74 @@
 :-use_module(library(pce)).
 :-pce_image_directory('./img').
+:- use_module(library(pce_style_item)).
 :-consult('./AgroSistemaExperto/motorInferencia.pl').
 resource(logo,image,image('logo.jpg')).
+resource(siembra,image,image('siembra.jpg')).
 
 mes:-write('Push it').
 
-plantar:-new(Dp,dialog('Vamos a plantar!')),
-new(TxtExt,text_item('Hortaliza')),
-new(TxtPrueba,text_item('Prueba')),
-new(MenuMes,menu('Que chingaos quieres?',cycle)),
-send_list(MenuMes,append,['Enero','Febrero','Plastico']),
-new(BtnHor,button('Buscar',message(@prolog,miProfundidad,TxtExt?selection,TxtPrueba))),
+obtenerPlantar(Hemisferio,Mes):-L = [Hemisferio,Mes],write(L).
 
-send_list(Dp,append,[TxtExt,TxtPrueba,MenuMes,BtnHor]),
+plantar:-new(Dp,dialog('Vamos a plantar!')),
+new(MenuMes,menu('mes:',cycle)),
+send_list(MenuMes,append,['Enero','Febrero','Marzo','Abril',
+	'Mayo','Junio','Julio','Agosto','Septiembre','Octubre','Novimbre','Diciembre']),
+
+new(Op, menu('Hemisferio', marked)),
+send(Op, append, norte), send(Op,append, sur),  
+send(Op, size,size(300,300)), 
+send(Dp, display, Op, point(100, 40)),
+%% new(BtnHor,button('Buscar',message(@prolog,obtenerPlantar,Op?selection,MenuMes?selection))),
+new(BtnHor,button('Buscar',message(@prolog,infoPlantar,MenuMes?selection))),
+
+send_list(Dp,append,[MenuMes,Op,BtnHor]),
 
 send(Dp,open).
 
-infoPlantado:-new(Di,dialog('Hortaliza')),
-%% new(LblHort)
-send_list(Di,append,[]),
-send(Di,open).
+
+infoPlantar(Mes):-
+%% write(Mes),
+%% miLista(Mes,Lista),
+%% write(Lista),
+new(Dinfo,dialog('Información')),
+new(MenuHor,menu('mes:',cycle)),
+send_list(MenuHor,append,['chile','acelga']),%%Cabiar a lista dinamica
+
+new(TxtProfundidad,text_item('Profundidad(cm)')),
+new(TxtGerminaUno,text_item('Inicio Germina(Dias)')),
+new(TxtGerminaDos,text_item('Fin Germina(Dias)')),
+new(TxtRecoleccionUno,text_item('Inicio Recoleccion(dias)')),
+new(TxtRecoleccionDos,text_item('Fin Recoleccion(dias)')),
+new(TxtDisPlant,text_item('Disntancia entre plantacas (cm)')),
+new(TxtDisHil,text_item('Disntancia entre hilera (cm)')),
+new(TxtInfo,text_item('Información')),
+new(LblSiembra,label(siembra,resource(siembra))),
+%% send(LblSiembra,right,TxtHortaliza),
+new(BtnVer,button('Ver',message(@prolog,miTodo,MenuHor?selection,
+	TxtProfundidad,TxtGerminaUno,TxtGerminaDos,TxtDisPlant,TxtDisHil,
+	TxtRecoleccionUno,TxtRecoleccionDos))),
+send_list(Dinfo,append,[LblSiembra,MenuHor,TxtProfundidad,TxtGerminaUno,TxtGerminaDos,TxtRecoleccionUno,
+	TxtRecoleccionDos,TxtDisPlant,TxtDisHil,TxtInfo,BtnVer]),
+%% send(Dinfo,display,point(650,300)),
+
+send(Dinfo,open_centered).
+
+%% info(X):-
 
 plantado:-new(Dpl,dialog('Titulo')),
-%% send(B,append,[chile,acelga]),
-%% send(B,append,Dpl),
+new(MenuHor,menu('Hortaliza',cycle)),
+send_list(MenuHor,append,['chile','acelga']),
+new(TxtIni,text_item('Inicio aprox. de plantacion')),
+new(TxtGerminaUno,text_item('Inicio Germina(Dias)')),
+new(TxtGerminaDos,text_item('Fin Germina(Dias)')),
+new(TxtRecoleccionUno,text_item('Inicio Recoleccion(dias)')),
+new(TxtRecoleccionDos,text_item('Fin Recoleccion(dias)')),
+new(BtnCalc,button('Calcular',message(@prolog,miPlantado,MenuHor?selection,
+	TxtIni?selection,TxtGerminaUno,TxtGerminaDos,TxtRecoleccionUno,TxtRecoleccionDos))),
+send_list(Dpl,append,[MenuHor,TxtIni,TxtGerminaUno,
+	TxtGerminaDos,TxtRecoleccionUno,TxtRecoleccionDos,
+	BtnCalc]),
+
 send(Dpl,open).
 
 
@@ -33,6 +78,23 @@ new(BtnA,button('Quieres plantar?',message(@prolog,plantar))),
 new(BtnB,button('Ya tengo plantado!',message(@prolog,plantado))),
 
 send_list(D,append,[LblIcon,BtnA,BtnB]),
-%% send(BtnB,below,BtnA),
-send(D,open).
+send(D,open_centered).
 
+prueba:-
+new(D, dialog('title')), 
+%% miProfundidad(acelga,X),
+%% new(TxtA,text_item('')),
+%% send(TxtA,selection,miProfundidad(acelga,X)),
+%% write('Alv'),nl,
+%% write(X),
+%% send(D, size, size(500,500)), 
+%% send(D, append,new(Op, menu(options, marked))), 
+%% send(Op, append, option1), send(Op,append, option2), 
+%% send(Op, append, option2), 
+%% send(Op, size,size(300,300)), 
+%% send(D, display, Op, point(100, 40)), 
+%% send(D,append(new(B1,button(ok, message(D, return, Op?selection))))), 
+%% send(D,display, B1, point(100, 100)), 
+%% send(D, append(button(cancel,message(D, return, @nil)))), 
+%% send(D, default_button(ok)),get(D,confirm, Rval),write(Rval),
+send(D,open).
--- motorInferencia.pl
+++ motorInferencia.pl
@@ -1,4 +1,8 @@
 :-consult('./AgroSistemaExperto/conocimiento.pl').
+const(X,L,[X|L]).
+const(X,[],[X]).
+conc([],L,L).
+conc([X|L1],L2,[X|L3]):-conc(L1,L2,L3).
 
 miTipoSiembra(H,R):-tipoSiembra(H,L),write(L),send(R,selection,L).
 
@@ -13,3 +17,35 @@ send(Dmin,selection,Min),send(Dmax,selection,Max).
 miRecoleccion(H,Dmin,Dmax):-recoleccion(H,Min,Max),
 send(Dmin,selection,Min),send(Dmax,selection,Max).
 %%Que da pendiente mes
+
+miTodo(H,P,Gu,Gd,Du,Dd,Ru,Rd):-miProfundidad(H,P),miGermina(H,Gu,Gd),
+miDistancia(H,Du,Dd),miRecoleccion(H,Ru,Rd).
+
+miPlantado(H,Calc,X,_,Y,_):-germina(H,Gu,Gd),recoleccion(H,Ru,Rd),
+Calc>Gu,Calc<Gd,
+write(Calc),nl,write(Gu),nl,write(Gd),
+send(X,selection,'Esta en fechas de germinar'),send(Y,selection,'Todavía no es fecha de cosecha').
+%% X = 'Esta en fechas de germinar',Y = 'Todavía no es fecha de cosecha'.
+listona(Mes,[L]):-mes(H,MesLista),member(Mes,MesLista),
+const(H,L,L).
+
+%% miLista(Mes,ListaAux,Lista):-mes(H,MesLista),
+%% member(Mes,MesLista),
+%% cons(H,ListaAux,Lista).
+%% obtenerHortaliza(Mes,H):-mes(H,MesLista),
+%% member(Mes,MesLista).
+%% lista(Mes,Lista):-obtenerHortaliza(Mes,H),
+%% cons(H,[H],Lista).
+
+%% conc([H],ListaAux,Lista),miLista(Mes,ListaAux,Lista).
+
+prueba:-new(D,dialog('titulo')),
+new(MenuHor,menu('mes:',cycle)),
+send_list(MenuHor,append,['acelga','chile']),
+new(LblProfundidad,label(nombre,'Hoal')),
+new(TxtGerminaUno,text_item('Germina')),
+new(TxtGerminaDos,text_item('Germian')),
+new(BtnA,button('Pushale',message(@prolog,miProfundidad,MenuHor?selection,LblProfundidad))),
+
+send_list(D,append,[MenuHor,LblProfundidad,TxtGerminaUno,TxtGerminaDos,BtnA]),
+send(D,open).
